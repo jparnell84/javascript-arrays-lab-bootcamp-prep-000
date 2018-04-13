b884ba320ea684ef5c4c8f53e7e40183fffccdf1
Jonathan Parnell <jonathan.parnell1@gmail.com>
2018-04-13 18:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name){
-  appendKitten("Garfield");
+  kittens.shift("Garfield");
   return kittens;
 }
